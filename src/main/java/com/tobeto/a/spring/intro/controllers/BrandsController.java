512f3ca56a3293dc92891954a0dc40df55819e61
Melisa <melisaselinmusellim@gmail.com>
2023-11-22 10:43:17 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/tobeto/a/spring/intro/controllers/BrandsController.java
+++ src/main/java/com/tobeto/a/spring/intro/controllers/BrandsController.java
@@ -1,51 +1,22 @@
 package com.tobeto.a.spring.intro.controllers;
-
-import com.tobeto.a.spring.intro.entities.Brand;
-import com.tobeto.a.spring.intro.repositories.BrandRepository;
+import com.tobeto.a.spring.intro.services.abstracts.BrandService;
+import com.tobeto.a.spring.intro.services.concretes.BrandManager;
+import com.tobeto.a.spring.intro.services.dtos.brand.requests.AddBrandRequest;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+// Single Responsibility
 @RestController
 @RequestMapping("api/brands")
 public class BrandsController {
-    // final => sadece ctor'da ilgili değerin ataması yapılabileceğini söyler.
-    private final BrandRepository brandRepository;
-    public BrandsController(BrandRepository brandRepository) {
-        this.brandRepository = brandRepository;
-    }
-    // Dependency Injection
+    // BAĞIMLILIKLAR DAİMA SOYUT NESNELER ÜZERİNDEN
+    private BrandService brandService;
 
-    // Spring IoC => DI Container
-
-
-    @GetMapping
-    public List<Brand> getAll() {
-        // findAll => hazır bir JPArepository fonk.
-        List<Brand> brands = brandRepository.findAll();
-        return brands;
-    }
-
-    @GetMapping("{id}")
-    public Brand getById(@PathVariable int id){
-        //Optional : Opsiyonel olarak Brand
-        //Verilen id ile bir veri varsa onu yoksa exception fırlat.
-        return brandRepository.findById(id).orElseThrow();
+    public BrandsController(BrandService brandService) {
+        this.brandService = brandService;
     }
 
     @PostMapping
-    public void add(@RequestBody Brand brand){
-        brandRepository.save(brand);
+    public void add(@RequestBody AddBrandRequest request){
+        brandService.add(request);
     }
-
-    @DeleteMapping("{id}")
-    public void delete(@PathVariable int id){
-        Brand brandtoDelete = brandRepository.findById(id).orElseThrow();
-        //exception fırlaması = bu satırın çalışmaması
-        // bu satıra geldiyse, bu id ile bir veri vardır.
-        brandRepository.delete(brandtoDelete);
-    }
-
-    //UPDATE ÖDEV ARAŞTIRMA
 }
-// http://localhost:8080/api/brands GET
